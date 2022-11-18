a121533a6a00281d16048d045ccdc9ee1f953a7f
Wojtek <ozarskiw@gmail.com>
2022-11-18 23:28:11 +0100

    Some obvious comments.

--- projektParadygmatyProgramowania.pl
+++ projektParadygmatyProgramowania.pl
@@ -10,6 +10,7 @@ coord(h, 8).
 queen(c, 3).
 queen(h, 6).
 
+%rule checking if square requested is on the same row/colum as a queen
 isThreatened(X1, Y1):-
     queen(X1, Y2);
     queen(X1, Y1);
@@ -20,6 +21,7 @@ isThreatened(X1, Y1):-
     NumValX1<9,
     NumValX1>0.
 
+%rule checking diagonals
 isThreatened(X1, Y1):-
     queen(X2, Y2),
     coord(CheckX1, Y1),
@@ -33,5 +35,9 @@ isThreatened(X1, Y1):-
     Y2<9,
     Y1>0,
     Y2>0,
+    %assigning DivVal 'direction' of a vector between
+    %queen on the board and chosen square
     DivVal is (abs(NumVal1-NumVal2)/abs(Y1-Y2)),
+    %if absolute value of 'direction' is 1 then chosen square 
+    %and the queen are on the same diagonal - square is threatened
     DivVal == 1.
